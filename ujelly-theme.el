c066af10ac41d2c459e113c5932ef6844193e7e5
Mark Tran <mark.tran@gmail.com>
2017-02-27 15:22:46 +0700

    add faces for highlight-indentation-mode

--- ujelly-theme.el
+++ ujelly-theme.el
@@ -2,7 +2,7 @@
 
 ;; Author: Mark Tran <mark.tran@gmail.com>
 ;; URL: http://github.com/marktran/color-theme-ujelly
-;; Version: 1.1.37
+;; Version: 1.1.38
 
 ;; Inspired by jellybeans: http://www.vim.org/scripts/script.php?script_id=2555.
 (deftheme ujelly "The ujelly color theme")
@@ -125,6 +125,8 @@
        `(helm-swoop-target-line-face ((,class (:foreground ,ujelly-fg :background ,ujelly-grey-4))))
        `(helm-swoop-target-word-face ((,class (:foreground ,ujelly-red-1))))
        `(highlight ((,class (:background ,ujelly-grey-4))))
+       `(highlight-indentation-face ((,class (:background ,ujelly-grey-4))))
+       `(highlight-indentation-current-column-face ((,class (:background ,ujelly-grey-4))))
        `(hl-line ((,class (:background ,ujelly-grey-4))))
        `(iedit-occurrence ((,class (:foreground ,ujelly-red-1))))
        `(isearch ((,class (:foreground ,ujelly-fg :background ,ujelly-red-1))))
