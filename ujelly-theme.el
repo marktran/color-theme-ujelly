0a4d4ce7fd3cbd580b60bc4ce83e0db4d39f54e5
Mark Tran <mark.tran@gmail.com>
2024-11-10 18:45:08 -0800

    bump version

--- ujelly-theme.el
+++ ujelly-theme.el
@@ -2,7 +2,7 @@
 
 ;; Author: Mark Tran <mark.tran@gmail.com>
 ;; URL: http://github.com/marktran/color-theme-ujelly
-;; Version: 1.3.3
+;; Version: 1.3.4
 
 ;; Inspired by jellybeans: http://www.vim.org/scripts/script.php?script_id=2555.
 (deftheme ujelly "The ujelly color theme")
